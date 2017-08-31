6151ce0700b4813cfffd6c0e1473f9e48488ebee
Jon Packer <contact@jonpacker.com>
2017-08-31 10:33:38 +0200

    fix day sorting

--- public/js/cbc.js
+++ public/js/cbc.js
@@ -106,14 +106,32 @@ function fetchUntappd(path, opts) {
 
 function calcBeerList(opts) {
   if (opts.order == 'live_rating') rankGroups.live_rating = orderByProp('live_rating');
+  if (opts.order == 'ut_rating') rankGroups.ut_rating = orderByProp('ut_rating');
+  
   var beers = beerSubsetWithRankings(function(beer) { 
     var match = true;
     if (opts.metastyle) match = match && beer.metastyle == opts.metastyle;
-    if (opts.colour) match = match && beer.session == opts.colour;
+    if (opts.colour) match = match && ((beer.sessions && beer.sessions.indexOf(opts.colour) != -1) || beer.session == opts.colour);
     if (opts.tasted) match = match && (opts.tasted == 'not-tasted' ? !beer.tasted : beer.tasted === opts.tasted);
     if (opts.saved) match = match && (opts.saved == 'not-saved' ? !beer.saved : beer.saved === opts.saved);
     return match;
   });
+  
+  if (!opts.colour) { 
+    beers = _.values(beers.reduce(function(unique, beer) {
+      if (unique[beer.id]) {
+        unique[beer.id].sessionSet.push(beer.session);
+      } else {
+        unique[beer.id] = beer;
+        beer.sessionSet = [beer.session];
+      }
+      return unique;
+    }, {})).map(function(beer) {
+      beer.sessionSet = beer.sessionSet.join(' ');
+      return beer;
+    });
+  }
+  
   var breweries = beers.reduce(function(breweries, beer) {
     var brewery = beer.brewery;
     if (opts.order) {
